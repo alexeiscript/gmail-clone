25972accdf34cf9e6a1b6d28605d40847127ccb6
alexeiscript <suzakukururug1@yandex.com>
2021-02-27 21:55:55 +0000

    populating sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,12 @@ import { Button } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 import InboxIcon from '@material-ui/icons/Inbox'
 import SidebarOption from './SidebarOption'
+import StarIcon from '@material-ui/icons/Star'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import LabelImportantIcon from '@material-ui/icons/LabelImportant'
+import NearMeIcon from '@material-ui/icons/NearMe'
+import NoteIcon from '@material-ui/icons/Note'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 
 function Sidebar() {
   return (
@@ -14,7 +20,54 @@ function Sidebar() {
       >
         Compose
       </Button>
-      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+      <SidebarOption 
+        Icon={InboxIcon} 
+        title="Inbox" 
+        number={54} 
+        selected={true} 
+      />
+
+      <SidebarOption 
+        Icon={StarIcon} 
+        title="Starred" 
+        number={54} 
+      
+      />
+
+      <SidebarOption 
+        Icon={AccessTimeIcon} 
+        title="Snoozed" 
+        number={54} 
+      
+      />
+
+      <SidebarOption 
+        Icon={LabelImportantIcon} 
+        title="Important" 
+        number={54} 
+      
+      />
+
+      <SidebarOption 
+        Icon={NearMeIcon} 
+        title="Sent" 
+        number={54} 
+      
+      />
+
+      <SidebarOption 
+        Icon={NoteIcon} 
+        title="Drafts" 
+        number={54} 
+      
+      />
+
+      <SidebarOption 
+        Icon={ExpandMoreIcon} 
+        title="More" 
+        number={54} 
+      
+      />  
     </div>
   )
 }
