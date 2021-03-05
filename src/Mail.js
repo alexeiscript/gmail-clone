5e6c85f2ada5c000cfb26eb4cf7f65258f6f5f36
alexeiscript <suzakukururug1@yandex.com>
2021-03-05 13:01:52 +0000

    adding icons

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,73 @@
 import React from 'react'
 import './Mail.css'
+import { IconButton } from '@material-ui/core'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import ErrorIcon from '@material-ui/icons/Error'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EmailIcon from '@material-ui/icons/Email'
+import WatchLaterIcon from '@material-ui/icons/WatchLater'
+import CheckCircleIcon from '@material-ui/icons/CheckCircle'
+import LabelImportantIcon from '@material-ui/icons/LabelImportant'
+import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import PrintIcon from '@material-ui/icons/Print'
+import { useHistory } from 'react-router-dom'
+
 
 function Mail() {
+  const history = useHistory()
+
   return (
     <div className="mail">
-      
+      <div className="mail_tools">
+        <div className="mail_toolsLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="mail_toolsRight">
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   )
 }
